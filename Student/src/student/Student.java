c7fe57a6a439f474120ff903b1c8c97a068897e2
Arun <Arun@192.168.137.142>
2025-06-17 11:51:28 +0530

    This is a java program

---
+++ Student/src/student/Student.java
@@ -0,0 +1,14 @@
+package student;
+
+public class Student {
+
+	public static void main(String[] args) {
+		Student a=new Student();
+		System.out.println(a);
+		Student s1=new Student();
+		System.out.println(s1);
+				
+
+	}
+
+}
